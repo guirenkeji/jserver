7d74ecdc58d2d3ff2800a2c6aba996e1e54b0d41
fls <you@example.com>
2017-03-22 14:18:05 +0800

    整理文件结构

--- src/controllers/__init__.py
+++ src/controllers/__init__.py
@@ -1,1 +1,5 @@
+# from src.controllers.svnprocontroller import svn_repo_info
+from src.controllers.zentaocontroller import zentao
+from src.controllers.nexuscontroller import nexusServer
+from src.controllers.agentAPI.nexus import nexus
 
